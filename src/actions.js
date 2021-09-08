df7dc65c71f4b7b02eb26603a9f36a859f788959
LearnToCode1138 <mdelgado1138@hotmail.com>
2021-09-07 20:10:06 -0700

    redux-thunk & async actions

--- src/actions.js
+++ src/actions.js
@@ -1,4 +1,9 @@
-import { CHANGE_SEARCH_FIELD } from './constants.js';
+import {
+  CHANGE_SEARCH_FIELD,
+  REQUEST_ROBOTS_PENDING,
+  REQUEST_ROBOTS_SUCCESS,
+  REQUEST_ROBOTS_FAILED,
+} from './constants.js';
 
 export const setSearchField = text => {
   // console.log(text);
@@ -6,4 +11,13 @@ export const setSearchField = text => {
     type: CHANGE_SEARCH_FIELD,
     payload: text,
   };
+};
+
+// syntax is necessary since redux-thunk expects a function to be returned by our action
+export const requestRobots = () => dispatch => {
+  dispatch({ type: REQUEST_ROBOTS_PENDING });
+  fetch('https://jsonplaceholder.typicode.com/users')
+    .then(response => response.json())
+    .then(data => dispatch({ type: REQUEST_ROBOTS_SUCCESS, payload: data }))
+    .catch(error => dispatch({ type: REQUEST_ROBOTS_FAILED, payload: error }));
 };
